a95ce2231eb6817e840a2ebba39ce95de9eff3d4
Ubuntu Git <ubuntu@example.com>
2020-02-23 18:58:16 +0300

    Initial terraform commit

---
+++ main.tf
@@ -0,0 +1,18 @@
+# comment line
+
+
+provider "aws" {
+  version = "~> 2.50"
+  region = "eu-central-1"
+}
+
+
+resource "aws_instance" "example" {
+  ami             = "ami-0df0e7600ad0913a9"
+  instance_type   = "t2.micro"
+  key_name        = "test-key-Frankfurt"
+
+  tags = {
+    Name = "terraform-example"
+  }
+}
