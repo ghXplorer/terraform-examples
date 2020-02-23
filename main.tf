65c36f4fef7ea9ee4769c71eb2d8c75a48b000fa
Ubuntu Git <ubuntu@example.com>
2020-02-23 21:10:30 +0300

    add user data shell script to run busybox httpd and security group config

--- main.tf
+++ main.tf
@@ -5,11 +5,33 @@ provider "aws" {
 
 
 resource "aws_instance" "example" {
-  ami             = "ami-0df0e7600ad0913a9"
-  instance_type   = "t2.micro"
-  key_name        = "test-key-Frankfurt"
+  ami                    = "ami-0df0e7600ad0913a9"
+  instance_type          = "t2.micro"
+  key_name               = "test-key-Frankfurt"
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              wget https://busybox.net/downloads/binaries/1.28.1-defconfig-multiarch/busybox-x86_64
+              mv busybox-x86_64 busybox
+              chmod +x busybox
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
   tags = {
     Name = "terraform-example"
+  }
+}
+
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
