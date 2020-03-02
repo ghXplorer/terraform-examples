57ca3c2ca1b24272c6822c844a1738056e9fed89
ghXplorer <57403493+ghXplorer@users.noreply.github.com>
2020-03-02 16:54:37 +0300

    Create del_version_enable_bucket.py

---
+++ del_version_enable_bucket.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3.7
+
+''' 
+This script deals with this terraform error: 
+Error: error deleting S3 Bucket (terraform-up-and-running-state-26022020): BucketNotEmpty:
+The bucket you tried to delete is not empty. You must delete all versions in the bucket.
+'''
+
+BUCKET = 'terraform-up-and-running-state-26022020'
+
+import boto3
+
+s3 = boto3.resource('s3')
+bucket = s3.Bucket(BUCKET)
+bucket.object_versions.delete()
+
+# if you want to delete the now-empty bucket as well, uncomment this line:
+bucket.delete()
